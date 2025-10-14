c1ec000c04f73f8b4a936ea0fffc868541f8a354
venkatasrikarakella <srikarvishnudatta@gmail.com>
2025-10-13 23:32:02 -0400

    fixed code to save to xlsx

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-
+import pandas
+import json
 load_dotenv()
 
 API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -18,6 +19,7 @@ extraction_prompt = PromptTemplate.from_template(
     """Extract the following information from this job posting and format it as JSON:
     {{
         "title": "job title here",
+        "companyName": "company name here",
         "location": "location here",
         "description": "full job description on the page"
     }}
@@ -48,7 +50,15 @@ def extract_job_info(content):
     """Extract structured job info using Gemini"""
     chain = extraction_prompt | llm
     result = chain.invoke({"content": content[:10000]})
-    return result.content
+
+    response = result.content.strip()
+    if response.startswith("```json"):
+        response = response[7:]  # Remove ```json
+    if response.startswith("```"):
+        response = response[3:]  # Remove ```
+    if response.endswith("```"):
+        response = response[:-3]  
+    return response.strip()
 
 def main():
     # read the job urls from txt file
@@ -57,14 +67,27 @@ def main():
         urls = [line.strip() for line in f if line.strip()]
     
     print("Processing the urls from the file")
-
+    rows = []
     for url in urls:
         try:
             content = scrape_job_page(url)
             job_info = extract_job_info(content)
-            print(job_info)
+            try:
+                data = json.loads(str(job_info))
+                rows.append([
+                    data.get("title", ""),
+                    data.get("companyName", ""),
+                    data.get("location", ""),
+                    data.get("description", ""),
+                    url
+                ])  
+            except Exception as ex:
+                print(f"JSON parse error: {ex}")
         except Exception as e:
             print(f"Error occured {e}")
+    
+    df = pandas.DataFrame(rows, columns=["Title", "Company Name", "Location", "Description", "Job Link"])
+    df.to_excel("extracted_jobs.xlsx", index=False)
 
 if __name__ == "__main__":
     main()
